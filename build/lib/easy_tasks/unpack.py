61886d20b5263086112a2968cbd2691e33251fe7
ICreedenI <andre.herber.programming@gmail.com>
2023-01-07 18:56:22 +0100

    pip

--- build/lib/easy_tasks/unpack.py
+++
@@ -1,9 +0,0 @@
-def unpack_list(List: list | tuple):
-    singles = []
-    for l in List:
-        if type(l) != str:
-            ls = unpack_list(l)
-            singles.extend(ls)
-        else:
-            singles.append(l)
-    return singles

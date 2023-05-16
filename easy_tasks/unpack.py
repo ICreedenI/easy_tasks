f6628c318cd112f2c2684d8c1b585927ade67de2
ICreedenI <andre.herber.programming@gmail.com>
2023-05-17 00:05:22 +0200

    unpack_list to unpack_iterable

--- easy_tasks/unpack.py
+++ easy_tasks/unpack.py
@@ -1,9 +1,18 @@
-def unpack_list(List: list | tuple):
+from typing import Iterable
+
+
+def unpack_iterable(iterabl: list | tuple):
+    """go from a nested iterable to one list containing every previously nested item; will not iterate on strings"""
     singles = []
-    for l in List:
-        if type(l) != str:
-            ls = unpack_list(l)
+    for l in iterabl:
+        if type(l) != str and isinstance(l, Iterable):
+            ls = unpack_iterable(l)
             singles.extend(ls)
         else:
             singles.append(l)
     return singles
+
+
+def unpack_list(List: list | tuple):
+    """go from a nested iterable to one list containing every previously nested item; will not iterate on strings\nis the same as unpack_iterable"""
+    return unpack_iterable(List)

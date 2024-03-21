2c80f76e81294f3008f227466a8a544e89ce0be0
ICreedenI <andre.herber.programming@gmail.com>
2024-03-21 11:50:12 +0100

    bug fix percentage precision probressbar

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ DESCRIPTION = (
 # Setting up
 setup(
     name="easy_tasks",
-    version="0.0.34",
+    version="0.0.35",
     author="André Herber",
     author_email="andre.herber.programming@gmail.com",
     # url="https://github.com/ICreedenI/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
